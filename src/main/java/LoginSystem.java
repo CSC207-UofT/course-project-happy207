8beff7a1bf414bb05b0cebbae3c5fea325423019
ryantheF <ryanj.fang@mail.utoronto.ca>
2021-11-10 19:39:13 -0500

    Added description

--- src/main/java/LoginSystem.java
+++ src/main/java/LoginSystem.java
@@ -3,7 +3,13 @@ import java.util.Objects;
 
 public class LoginSystem {
 
-    public static boolean login(String input) throws IOException {
+    /**
+     *
+     * @param username With the given file, check if the username exists or not.
+     * @return return true it username exist, return false otherwise.
+     * @throws IOException throws exception if error occurs during file reading.
+     */
+    public static boolean checkUsername(String username) throws IOException {
         String path = "users.csv";
 
         BufferedReader br =new BufferedReader(new FileReader(path));
@@ -11,23 +17,29 @@ public class LoginSystem {
 
         while ((line = br.readLine()) != null){
             String[] data = line.split(",");
-            if (Objects.equals(data[0], input)){
+            if (Objects.equals(data[0], username)){
                 return true;
             }
         }
         return false;
     }
 
-    public static void addData(String username, String password, String fullname, String file){
+    /**
+     *
+     * @param username New username
+     * @param password Password that matches the new username
+     * @param fullName fullName of this user with username
+     * @param file  data file that stores all users.
+     */
+    public static void addData(String username, String password, String fullName, String file){
         try {
             FileWriter fw = new FileWriter(file, true);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter pw = new PrintWriter(bw);
 
-            pw.println(username + "," + password + "," + fullname);
+            pw.println(username + "," + password + "," + fullName);
             pw.flush();
             pw.close();
-
             System.out.println("success");
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,8 +48,8 @@ public class LoginSystem {
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(login("Ryan"));
+        System.out.println(checkUsername("Ryan"));
         addData("HOLA", "mypassword", "HOLALA", "users.csv");
-        System.out.println(login("HOLA"));
+        System.out.println(checkUsername("HOLA"));
     }
 }
